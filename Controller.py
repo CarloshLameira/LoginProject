1e35406c3c508778fef1d996b17ccf697568cc3c
CarloshLameira <carloshlameira@hotmail.com>
2022-02-01 18:18:45 -0300

    Modificação com hash

--- Controller.py
+++ Controller.py
@@ -1,9 +1,9 @@
 from Model import User
 import rsa
 from Dal import DalUser
-from rsa.key import PrivateKey, PublicKey
 import re
 from hashlib import sha256
+
 
 def check(email):
         '''ESSA FUNCAO VERIFICA SE UMA STRING ESTA NO FORMATO DE UM EMAIL
@@ -22,8 +22,7 @@ def check(email):
          
 
 class UserController:
-        #Keypublic = PublicKey(8959256223667307440646239669639551691754589213337493349186493242210461639848394061610513627742212005523399981863814891297358119440031491390825189564189331, 65537)    
-        #KeyPrivate = PrivateKey(8959256223667307440646239669639551691754589213337493349186493242210461639848394061610513627742212005523399981863814891297358119440031491390825189564189331, 65537, 6081472639364997464097969359958269322053109781414036790391844887208364682617742026209852044088196436089392145563399114872145083288864215743920253430390769, 6869053162681910458482422563820092584873136287379889387845611634777872352647051437, 1304292747702262807886140354689988773602858015335320497101886396934502463)
+        
         @classmethod
         def cadastrar(cls,name, email, password):
                 ''' ESSE METODO TESTA SE OS VALORES DE NOME/EMAIL/SENHA SAO VALIDOS
@@ -48,8 +47,8 @@ class UserController:
                                                 if re.search("[A-Z]", password):
                                                         if re.search("[0-9]", password):
                                                                 if re.search("[_@$!?/|\#%&]", password):
-                                                                        encpassword = sha256(password.encode())
-                                                                        user = User(user_name=name, user_email=email, user_pass=encpassword)
+                                                                        hashpassword = sha256(password.encode()).hexdigest()
+                                                                        user = User(user_name=name, user_email=email, user_pass=hashpassword)
                                                                         DalUser.cadastrar(user)
                                                                         return 'Usuario Cadastrado'
                                                                 else:
@@ -74,14 +73,12 @@ class UserController:
                 Return: String
                 '''
                 x = DalUser.find(email)
+                hashpass = sha256(password.encode()).hexdigest()
                 for i in x:
-                        decMessage = rsa.decrypt(i.user_pass, cls.KeyPrivate).decode()
-                        if decMessage == password:
+                        if i.user_pass == hashpass:
                                 return 'Logado com sucesso'
                         else:
                                 return 'Senha invalida'
 
                 
 
-#print(UserController.login('ze@gmail.com', 'Ventilador10!'))
-#print(UserController.cadastrar('ze', 'zezinnn@gmail.com', 'Ventilador10!'))
--- Model.py
+++ Model.py
@@ -1,14 +1,14 @@
-from sqlalchemy import Column, Integer, String,BLOB
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
+Base = declarative_base()#importa codigos de banco de dados que o sqlalchemy usa
 
 class User(Base):
         '''DEFINE A CLASSE/TABELA USER
         '''
         __tablename__ = 'User'
         user_id = Column(Integer, primary_key=True)
-        user_name = Column(String(500))
+        user_name = Column(String(60))
         user_email = Column(String(35))
-        user_pass = Column(BLOB())
+        user_pass = Column(String(64))
 
